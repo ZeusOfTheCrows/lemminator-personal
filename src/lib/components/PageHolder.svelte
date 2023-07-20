7c489e7af8e38624e15eeff00ac0404cc939c0ad
FrostySpectacles <>
2023-07-20 20:54:13 +0200

    Make page content slottable

---
+++ src/lib/components/PageHolder.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import PrimarySidebar from '$lib/components/PrimarySidebar.svelte';
+</script>
+
+<div class="pageHolder">
+	<aside class="primarySidebar">
+		<PrimarySidebar />
+	</aside>
+	<main class="main">
+		<slot name="main" />
+	</main>
+	<aside class="secondarySidebar">
+		<slot name="secondarySidebar" />
+	</aside>
+</div>
+
+<style lang="scss">
+	@use '$lib/css/resets';
+	@use '$lib/css/globals';
+	@use '$lib/css/colors';
+
+	.pageHolder {
+		display: flex;
+		flex-direction: row;
+		align-items: start;
+		min-height: 100%;
+
+		.primarySidebar {
+			padding: 1rem 0;
+			position: sticky;
+			top: 0;
+			max-width: 300px;
+			flex-basis: 300px;
+			flex-shrink: 0;
+		}
+
+		.main {
+			padding: 1rem;
+			align-self: stretch;
+			flex-grow: 1;
+			border-left: solid 1px colors.$subtleBorder;
+			border-right: solid 1px colors.$subtleBorder;
+		}
+
+		.secondarySidebar {
+			padding: 1rem;
+			position: sticky;
+			top: 0;
+			flex-basis: 300px;
+			flex-shrink: 0;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,14 @@
 <script lang="ts">
-	import PrimarySidebar from '$lib/components/PrimarySidebar.svelte';
 	import PostList from '$lib/components/PostList.svelte';
 	import SecondarySidebar from '$lib/components/SecondarySidebar.svelte';
+	import PageHolder from '$lib/components/PageHolder.svelte';
 </script>
 
-<div class="rootLayout">
-	<aside class="primarySidebar">
-		<PrimarySidebar />
-	</aside>
-	<main class="main">
+<PageHolder>
+	<svelte:fragment slot="main">
 		<PostList />
-	</main>
-	<aside class="secondarySidebar">
+	</svelte:fragment>
+	<svelte:fragment slot="secondarySidebar">
 		<SecondarySidebar />
-	</aside>
-</div>
-
-<style lang="scss">
-	@use '$lib/css/resets';
-	@use '$lib/css/globals';
-	@use '$lib/css/colors';
-
-	.rootLayout {
-		display: flex;
-		flex-direction: row;
-		align-items: start;
-		min-height: 100%;
-
-		.primarySidebar {
-			padding: 1rem 0;
-			position: sticky;
-			top: 0;
-			max-width: 300px;
-			flex-basis: 300px;
-			flex-shrink: 0;
-		}
-
-		.main {
-			padding: 1rem;
-			align-self: stretch;
-			flex-grow: 1;
-			border-left: solid 1px colors.$subtleBorder;
-			border-right: solid 1px colors.$subtleBorder;
-		}
-
-		.secondarySidebar {
-			padding: 1rem;
-			position: sticky;
-			top: 0;
-			flex-basis: 300px;
-			flex-shrink: 0;
-		}
-	}
-</style>
+	</svelte:fragment>
+</PageHolder>
