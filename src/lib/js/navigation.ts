5e4f0873d882780f65ceee1124bbae3c1a426120
FrostySpectacles <>
2023-07-29 22:17:21 +0200

    Move post URLs
    I'm planning to re-implement community API pages as separate pages in
    the UI as opposed to an endlessly growing list. The latter is a bit
    problematic when opening posts beyond page 1, for people using mouse
    navigation as well as people using keyboard.
    
    Due to this, we'll need /c/[communityName]/page/[pageId] to be
    available.

--- src/lib/js/navigation.ts
+++ src/lib/js/navigation.ts
@@ -1,5 +1,5 @@
 import type { PostView } from "lemmy-js-client";
 
 export function getDetailLinkForPost(postView: PostView): string {
-    return `/c/${postView.community.name}/${postView.post.id}`;
+    return `/c/${postView.community.name}/post/${postView.post.id}`;
 }
