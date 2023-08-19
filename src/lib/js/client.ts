7286577b25683e4e47fce4abb6a1fc47bdcfc976
FrostySpectacles <>
2023-08-19 13:17:56 +0200

    Remove obsolete instrumentation

--- src/lib/js/client.ts
+++ src/lib/js/client.ts
@@ -8,13 +8,12 @@ import { TimeoutError, timeout } from 'promise-timeout';
 class ApiClient {
     constructor(private innerClient: LemmyHttp) { }
 
-    private async wrapForApiTimeouts<T>(callerDescription: string, promise: Promise<T>): Promise<T> {
+    private async wrapForApiTimeouts<T>(promise: Promise<T>): Promise<T> {
         // Server-side rendering is on a tight deadline. For client-side rendering we can wait
         // however long we want as long as it still makes sense.
         const maxDelay = browser ? 20 * 1000 : 8 * 1000;
 
         return timeout(promise, maxDelay).catch((err) => {
-            console.error(`Error in ${callerDescription}`);
             if (err instanceof TimeoutError) {
                 throw error(504, 'Server request timed out');
             } else {
@@ -24,7 +23,7 @@ class ApiClient {
     }
 
     async acquireJwt(usernameOrEmail: string, password: string): Promise<string> {
-        const response = await this.wrapForApiTimeouts('acquireJwt', this.innerClient.login({
+        const response = await this.wrapForApiTimeouts(this.innerClient.login({
             username_or_email: usernameOrEmail,
             password,
         }));
@@ -34,7 +33,7 @@ class ApiClient {
     }
 
     listTopCommunities(): Promise<ListCommunitiesResponse> {
-        return this.wrapForApiTimeouts('listTopCommunities', this.innerClient
+        return this.wrapForApiTimeouts(this.innerClient
             .listCommunities({
                 type_: 'Local',
                 sort: 'TopDay'
@@ -42,20 +41,20 @@ class ApiClient {
     }
 
     getSite(jwt?: string): Promise<GetSiteResponse> {
-        return this.wrapForApiTimeouts('getSite', this.innerClient.getSite({
+        return this.wrapForApiTimeouts(this.innerClient.getSite({
             auth: jwt,
         }));
     }
 
     getCommunity(communityName: string, jwt?: string): Promise<GetCommunityResponse> {
-        return this.wrapForApiTimeouts('getCommunity', this.innerClient.getCommunity({
+        return this.wrapForApiTimeouts(this.innerClient.getCommunity({
             auth: jwt,
             name: communityName,
         }));
     }
 
     getPosts(communityName: string | undefined, pageIdNum: number, jwt?: string): Promise<GetPostsResponse> {
-        return this.wrapForApiTimeouts('getPosts', this.innerClient.getPosts({
+        return this.wrapForApiTimeouts(this.innerClient.getPosts({
             auth: jwt,
             page: pageIdNum,
             limit: POST_PAGE_SIZE,
@@ -64,14 +63,14 @@ class ApiClient {
     }
 
     getPost(post_id: number, jwt?: string): Promise<GetPostResponse> {
-        return this.wrapForApiTimeouts('getPost', this.innerClient.getPost({
+        return this.wrapForApiTimeouts(this.innerClient.getPost({
             auth: jwt,
             id: post_id,
         }));
     }
 
     voteOnPost(postId: number, score: 1 | 0 | -1, jwt: string): Promise<PostResponse> {
-        return this.wrapForApiTimeouts('voteOnPost', this.innerClient.likePost({
+        return this.wrapForApiTimeouts(this.innerClient.likePost({
             auth: jwt,
             post_id: postId,
             score,
@@ -79,7 +78,7 @@ class ApiClient {
     }
 
     voteOnComment(commentId: number, score: 1 | 0 | -1, jwt: string): Promise<CommentResponse> {
-        return this.wrapForApiTimeouts('voteOnComment', this.innerClient.likeComment({
+        return this.wrapForApiTimeouts(this.innerClient.likeComment({
             auth: jwt,
             comment_id: commentId,
             score,
@@ -87,14 +86,14 @@ class ApiClient {
     }
 
     getComment(params: { commentId: number, jwt?: string }): Promise<CommentResponse> {
-        return this.wrapForApiTimeouts('getComment', this.innerClient.getComment({
+        return this.wrapForApiTimeouts(this.innerClient.getComment({
             id: params.commentId,
             auth: params.jwt,
         }));
     }
 
     getComments(postId?: number, parentCommentId?: number, jwt?: string, page?: number): Promise<GetCommentsResponse> {
-        return this.wrapForApiTimeouts('getComments', this.innerClient.getComments({
+        return this.wrapForApiTimeouts(this.innerClient.getComments({
             auth: jwt,
             post_id: postId,
             parent_id: parentCommentId,
@@ -106,7 +105,7 @@ class ApiClient {
     }
 
     postComment(params: { postId: number, parentId?: number, content: string, jwt: string }): Promise<CommentResponse> {
-        return this.wrapForApiTimeouts('postComment', this.innerClient.createComment({
+        return this.wrapForApiTimeouts(this.innerClient.createComment({
             post_id: params.postId,
             parent_id: params.parentId,
             content: params.content,
@@ -115,7 +114,7 @@ class ApiClient {
     }
 
     editComment(params: { commentId: number, content: string, jwt: string }): Promise<CommentResponse> {
-        return this.wrapForApiTimeouts('editComment', this.innerClient.editComment({
+        return this.wrapForApiTimeouts(this.innerClient.editComment({
             comment_id: params.commentId,
             content: params.content,
             auth: params.jwt,
@@ -126,7 +125,7 @@ class ApiClient {
         commentId: number,
         jwt: string,
     }): Promise<CommentResponse> {
-        return this.wrapForApiTimeouts('deleteComment', this.innerClient.deleteComment({
+        return this.wrapForApiTimeouts(this.innerClient.deleteComment({
             comment_id: params.commentId,
             deleted: true,
             auth: params.jwt,
@@ -134,14 +133,14 @@ class ApiClient {
     }
 
     search(params: { query: string, jwt?: string }): Promise<SearchResponse> {
-        return this.wrapForApiTimeouts('searchCommunities', this.innerClient.search({
+        return this.wrapForApiTimeouts(this.innerClient.search({
             q: params.query,
             auth: params.jwt,
         }));
     }
 
     getSubscribedPosts(params: { pageId: number, jwt: string }) {
-        return this.wrapForApiTimeouts('getSubscribedPosts', this.innerClient.getPosts({
+        return this.wrapForApiTimeouts(this.innerClient.getPosts({
             page: params.pageId,
             auth: params.jwt,
             type_: 'Subscribed',
@@ -149,7 +148,7 @@ class ApiClient {
     }
 
     setSubscriptionStatus(params: { communityId: number, status: boolean, jwt: string }) {
-        return this.wrapForApiTimeouts('updateSubscriptionStatus', this.innerClient.followCommunity({
+        return this.wrapForApiTimeouts(this.innerClient.followCommunity({
             community_id: params.communityId,
             follow: params.status,
             auth: params.jwt,
@@ -157,20 +156,20 @@ class ApiClient {
     }
 
     getUnreadCount(params: { jwt: string }) {
-        return this.wrapForApiTimeouts('getUnreadCount', this.innerClient.getUnreadCount({
+        return this.wrapForApiTimeouts(this.innerClient.getUnreadCount({
             auth: params.jwt,
         }))
     }
 
     getReplies(params: { jwt: string }) {
-        return this.wrapForApiTimeouts('getReplies', this.innerClient.getReplies({
+        return this.wrapForApiTimeouts(this.innerClient.getReplies({
             unread_only: false,
             auth: params.jwt,
         }));
     }
 
     setCommentReadStatus(params: { commentReplyId: number, readStatus: boolean, jwt: string }) {
-        return this.wrapForApiTimeouts('setCommentReadStatus', this.innerClient.markCommentReplyAsRead({
+        return this.wrapForApiTimeouts(this.innerClient.markCommentReplyAsRead({
             comment_reply_id: params.commentReplyId,
             auth: params.jwt,
             read: params.readStatus,
